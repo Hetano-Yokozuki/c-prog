94627045b0965d8afbbd6cccc2e1628cebef779c
Hetano-Yokozuki <k.shiraki789@gmail.com>
2018-03-18 09:36:13 -0400

    defined a function

---
+++ 801.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+//defining a function named "buy"
+int buy(void)
+{
+    printf("bought a car \n");
+
+}
+
+//main function starts from here
+int main(void)
+{
+    buy();
+    return 0;
+}
